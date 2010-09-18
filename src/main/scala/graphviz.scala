0d6448d8563a0564ccb9973180bd79b532e007e6
Alexander Bernauer <alex@copton.net>
2010-09-19 00:19:44 +0200

    added references to the graph

--- src/main/scala/graphviz.scala
+++ src/main/scala/graphviz.scala
@@ -6,14 +6,21 @@ object Printer extends backend.Printer {
     for {
       node <- nodes
       father <- node.fathers
-    } yield "\"%s\" -> \"%s\" [arrowhead=empty];".format(node.name, father)
+    } yield "\"%s\" -> \"%s\" [arrowhead=empty,samehead=\"%s_father\"];".format(node.name, father, father)
 
   private def printChildren(nodes: Seq[ast.Node]): Seq[String] =
     for {
       node <- nodes
       child <- node.children
-    } yield "\"%s\" -> \"%s\" [arrowhead=diamond, taillabel=\"%s\", label=\"%s\"];".format(
-      node.name, child.target, child.cardinality, child.name)
+    } yield "\"%s\" -> \"%s\" [arrowhead=diamond, taillabel=\"%s\", label=\"%s\", samehead=\"%s_child\"];".format(
+      node.name, child.target, child.cardinality, child.name, child.target)
+
+  private def printReferences(nodes: Seq[ast.Node]): Seq[String] =
+    for {
+      node <- nodes
+      child <- node.references
+    } yield "\"%s\" -> \"%s\" [arrowhead=ediamond, taillabel=\"%s\", label=\"%s\", samehead=\"%s_reference\"];".format(
+      node.name, child.target, child.cardinality, child.name, child.target)
 
   private def printNodes(nodes: Seq[ast.Node]): Seq[String] =
     nodes map ((n) => n.typ match {
@@ -33,6 +40,7 @@ object Printer extends backend.Printer {
       printNodes(nodes) ++
       printInheritance(nodes) ++
       printChildren(nodes) ++
+      printReferences(nodes) ++
       List("}", "")
     ).mkString("\n")
 }
