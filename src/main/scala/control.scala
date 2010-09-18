93979d590129a0dfde6daf8a9f83df022e054f5e
Alexander Bernauer <alex@copton.net>
2010-09-18 12:21:11 +0200

    text backend

--- src/main/scala/control.scala
+++ src/main/scala/control.scala
@@ -1,47 +1,6 @@
 import frontend.Parser
 import scala.xml.XML
 
-class Options(
-  val infile: String,
-  val backend: String,
-  val outfile: String
-)
-
-object Options {
-  def apply(args: Array[String]): Options = {
-    var infile: Option[String] = None
-    var backend: Option[String] = None
-    var outfile: Option[String] = None
-
-    var i = 0
-    while(i+1 < args.size) {
-      args(i) match {
-        case "-i" => infile = Some(args(i+1))
-        case "-b" => backend = Some(args(i+1))
-        case "-o" => outfile = Some(args(i+1))
-        case _ => usage()
-      }
-      i += 2
-    }
-    if (i == args.size -1 || ! infile.isDefined || ! backend.isDefined || ! outfile.isDefined) {
-      usage()
-    } else {
-      new Options(infile.get, backend.get, outfile.get)
-    }
-  }
-
-  def usage(): Nothing = {
-    Console.print("""valid parameters:
--i <filename>: the input XML file according to MPS schema for language structures
--b <backend>: the backend. Possible values are: text, graphviz
--o <filename|-> the output file. - means standard output.
-""")
-    System.exit(1)
-    throw null // System.exit returns Unit but we need Nothing.
-  }
-}
-
-
 object Control {
   def main(args:Array[String]) = {
     val options = Options(args)
--- src/main/scala/text.scala
+++ src/main/scala/text.scala
@@ -2,7 +2,30 @@ package backend
 package text
 
 object Printer extends backend.Printer {
-  private def printNode(node: ast.Node): String = node.toString
+  private def print(property: ast.Property): String =
+    property.name + ": " + property.typ
+
+  private def print(association: ast.Association): String =
+    association.target + " (" + association.cardinality + ")"
+
+  private def print(father: String): String = 
+    father
+
+  private def print(typ: ast.NodeType.Value): String =
+    typ match {
+      case ast.NodeType.Concept => "Concept"
+      case ast.NodeType.InterfaceConcept => "InterfaceConcept"
+    }
+
+  private def printNode(node: ast.Node): String = List(
+      node.name,
+      "\ttype: " + print(node.typ),
+      "\tfathers: " + (node.fathers map print).mkString(", "),
+      "\tchildren: " + (node.children map print).mkString(", "),
+      "\treferences: " + (node.references map print).mkString(", "),
+      "\tproperties: " + (node.properties map print).mkString(", ")
+    ).mkString("\n")
+    
 
   def print(nodes: Seq[ast.Node]): String =
     (nodes map printNode).mkString("\n")
